cc473e7879b637e24d3944e5bb9535c8e2927bdb
Masaki Nabeshima <masakinabeshima@Masakis-MacBook-Pro-2.local>
2022-03-02 07:14:41 +0100

    add problem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+/*
+Example of what contacts object looks like:
+    let contacts = {
+        'Joe': 234723481,
+        'Mike': 723896498,
+        'Hanna': 876124322,
+    }
+*/
+
+function contactsToNames(contacts) {
+    return Object.keys(contacts);
+}
