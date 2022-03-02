9ea8621a8731ddb5ce43966c0e2eb87de25f64df
Masaki Nabeshima <masakinabeshima@Masakis-MacBook-Pro-2.local>
2022-03-02 08:16:59 +0100

    add problem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// This code compiles, but throws error during runtime. Is there a bad types at use here?
+
+type APIResponse<T> = {
+    status: number,
+    data: T
+};
+
+function getDataFromResponse<T>(response: APIResponse<T>): any {
+    return response.data;
+}
+
+const response = {
+    status: 200,
+    data: {message: 'ok'}
+}
+
+const data = getDataFromResponse(response);
+
+data.user.name = 'new user name';
